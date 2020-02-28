30f3dc09bc3ea93cf6ed0ecb2f3ea91e267171af
ccolombijn <ccolombijn@gmail.com>
2020-02-28 12:47:03 +0100

    Added helloworld demo, renamed app.json to rz.json

--- app/config.js
+++ app/config.js
@@ -2,17 +2,17 @@
 * app/config.js
 */
 'use strict';
-const _config = require('../config/app.json');
-function config(){
-  _config.ref_adr = process.env.REF_ADR;
-  _config.version = require('../package.json').version;
-  _config.npm_lifecycle_event = process.env.npm_lifecycle_event;
-  _config.ref_ws_port = process.env.REF_WS_PORT;
+const rzConfig = require('../config/rz.json');
+module.exports = ()=>{
+  rzConfig.ref_adr = process.env.REF_ADR;
+  rzConfig.version = require('../package.json').version;
+  rzConfig.npm_lifecycle_event = process.env.npm_lifecycle_event;
+  rzConfig.ref_ws_port = process.env.REF_WS_PORT;
   const models = require('../models');
-  _config.models = new Object({});
+  rzConfig.models = new Object({});
   for(let model of Object.getOwnPropertyNames(models.sequelize.models)){
-    _config.models[model] = models[model].rawAttributes;
+    rzConfig.models[model] = models[model].rawAttributes;
   }
-  return _config;
+  return rzConfig;
 }
-module.exports = config;
+
---
+++ assets/js/modules/helloworld.js
@@ -0,0 +1,9 @@
+const demoAppHelloWorld = {
+    name : 'helloworld',
+    template : 'helloworld',
+    helloworld : 'Hello World!',
+    default : ()=>{
+        console.log('Hello World!');
+    }
+};
+rz.add(demoAppHelloWorld);
